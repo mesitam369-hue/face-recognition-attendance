0aa852a8bf7c4b62223569ad601efe35ec3f93c1
mesitam369-hue <mesitam369@gmail.com>
2026-03-29 10:12:25 +0530

    Update camera_utils.py

--- camera_utils.py
+++ camera_utils.py
@@ -9,9 +9,9 @@ import base64
 class VideoCamera:
     def __init__(self):
         try:
-        self.video = cv2.VideoCapture(0)
+         self.video = cv2.VideoCapture(0)
         except:
-        self.last_face_locations = []
+         self.last_face_locations = []
         self.last_frame = None
         self.success = False
         self.stopped = False
